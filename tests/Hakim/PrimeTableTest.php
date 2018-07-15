c4b54be6f5c348427001e6bd6e67ba517720134a
Hakim Miah <hakim.miah.7214@gmail.com>
2018-07-15 17:42:33 +0100

    Test - check if error message given if no command line argument given

--- tests/Hakim/PrimeTableTest.php
+++ tests/Hakim/PrimeTableTest.php
@@ -6,5 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class PrimeTableTest extends TestCase
 {
-    
+    public function testArgumentGiven()
+    {
+        $output = exec('php main.php');
+        $this->assertEquals($output,'No argument supplied.  Please give an integer (whole number), 1 or greater.'); 
+    }
+
 }
