d02e96da6b44ae11997351106c8e9a5b6475d477
Hakim Miah <hakim.miah.7214@gmail.com>
2018-07-15 18:01:02 +0100

    Test - validNumeric function

--- tests/Hakim/PrimeTableTest.php
+++ tests/Hakim/PrimeTableTest.php
@@ -11,5 +11,16 @@ class PrimeTableTest extends TestCase
         $output = exec('php main.php');
         $this->assertEquals($output,'No argument supplied.  Please give an integer (whole number), 1 or greater.'); 
     }
+    
+    public function testValidNumeric()
+    {
+        $result = PrimeTable::validNumeric("foo");
+        $this->assertEquals($result,false);
+        
+        $result = PrimeTable::validNumeric("3");
+        $this->assertEquals($result,true);
+        
+        //etc
+    }
 
 }
