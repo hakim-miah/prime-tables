c8ddde81044bd31fb351fbd0c9fcefba863940a5
Hakim Miah <hakim.miah.7214@gmail.com>
2018-07-15 21:51:37 +0100

    Fix - wrong agument index

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 namespace PrimeTables\Hakim;
 require "vendor/autoload.php";
 
-if(!PrimeTable::validNumeric($argv[0])){
+if(empty($argv[1]) || !PrimeTable::validNumeric($argv[1])){
     echo PrimeTable::ERROR_MSG_INVALID_NUMERIC;
     exit(-1);
 }
